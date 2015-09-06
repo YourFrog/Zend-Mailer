9e02a09583e7df2f19cf1a672559059c67ef65d5
Paweł <p.stelmasiak@4medicine.pl>
2015-09-06 22:46:17 +0200

    Test
    Testowy commit

---
+++ Module.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace ZendMailer;
+
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
+
+/**
+ *  Klasa inicujca moduł mailera
+ *
+ * @package ZendMailer
+ */
+class Module
+{
+    public function onBootstrap(MvcEvent $e)
+    {
+        $eventManager        = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return [
+            'Zend\Loader\StandardAutoloader' => [
+                'namespaces' => [
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ],
+            ],
+        ];
+    }
+}
